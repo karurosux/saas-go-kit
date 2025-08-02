fe5751416092ef6af59e3d0ef178de4b40d45d1e
Carlos Gonzalez <justdevelopitmx@proton.me>
2025-08-03 06:14:31 +0700

    Integrated goreleaser

--- cmd/sgk/commands/version.go
+++ cmd/sgk/commands/version.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"runtime"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
@@ -19,10 +20,40 @@ func VersionCmd() *cobra.Command {
 		Short: "Print the version information",
 		Long:  "Display version, commit hash, build date, and runtime information",
 		Run: func(cmd *cobra.Command, args []string) {
+			version := Version
+			commit := Commit
+			buildDate := BuildDate
+			
+			if info, ok := debug.ReadBuildInfo(); ok && version == "dev" {
+				if info.Main.Version != "" && info.Main.Version != "(devel)" {
+					version = info.Main.Version
+				}
+				
+				for _, setting := range info.Settings {
+					switch setting.Key {
+					case "vcs.revision":
+						if commit == "none" && setting.Value != "" {
+							commit = setting.Value
+							if len(commit) > 7 {
+								commit = commit[:7]
+							}
+						}
+					case "vcs.time":
+						if buildDate == "unknown" && setting.Value != "" {
+							buildDate = setting.Value
+						}
+					case "vcs.modified":
+						if setting.Value == "true" && version != "dev" {
+							version += "-dirty"
+						}
+					}
+				}
+			}
+			
 			fmt.Printf("SGK (SaaS Go Kit) CLI\n")
-			fmt.Printf("Version:    %s\n", Version)
-			fmt.Printf("Commit:     %s\n", Commit)
-			fmt.Printf("Built:      %s\n", BuildDate)
+			fmt.Printf("Version:    %s\n", version)
+			fmt.Printf("Commit:     %s\n", commit)
+			fmt.Printf("Built:      %s\n", buildDate)
 			fmt.Printf("Go version: %s\n", runtime.Version())
 			fmt.Printf("OS/Arch:    %s/%s\n", runtime.GOOS, runtime.GOARCH)
 		},
